58c32b677ea38792a7ddd4d3ce407268882bbf0d
Beben <batarabentar5@gmail.com>
2025-12-18 15:28:38 +0700

    update app

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const bodyParser = require('body-parser')
-const userRoute = require('./routers/userRoute')
+const bodyparser = require('body-parser')
+const userroute = require('./routers/userroute')
+const userlogger = require('./middleware/logger')
 dotenv.config()
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use('/api/books',userRoute)
+app.use(bodyparser.json())
+app.use(userlogger)
+app.use('/api/books', userroute)
 
 const PORT = process.env.PORT
 
-app.listen(PORT, ()=>{
+app.listen(PORT,()=>{
     console.log(`http://localhost:${PORT}/api/books`);
-    
 })
